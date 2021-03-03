bea928fc32f2f1fddbd5fa2c628578bfcf8e8751
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-03-03 23:05:33 +0530

    Update BatteryHealthChecker.h

--- BatteryHealthChecker.h
+++ BatteryHealthChecker.h
@@ -1,3 +1,6 @@
+#ifndef BatteryHealthChecker_h
+#define BatteryHealthChecker_h
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
@@ -5,3 +8,5 @@
 extern int CheckAttributeRange(float attribute, float lowerthreshold, float higherthreshold);
 extern int LowerThresholdChecker(float attribute, float lowerthreshold);
 extern int UpperThresholdChecker(float attribute, float upperthreshold);
+
+#endif
